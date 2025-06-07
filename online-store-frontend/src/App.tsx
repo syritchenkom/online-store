8fddbedc9bb637b710cf68b1f44a1f3ef257527b
syritchenkom <syritchenko@gmail.com>
2025-06-07 10:33:29 +0200

    Modified files

--- online-store-frontend/src/App.tsx
+++ online-store-frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { AppRouter } from './components/AppRouter';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { AppRouter } from "./components/AppRouter";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <AppRouter />
+      <AppRouter />
     </BrowserRouter>
     // <div className="App">
     //   <header className="App-header">
@@ -15,4 +15,17 @@ function App() {
     //     <p>
     //       Edit <code>src/App.tsx</code> and save to reload.
     //     </p>
-    //     <a 
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
